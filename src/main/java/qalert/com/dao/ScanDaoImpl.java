9c33be065d302b417e73163e241812830a9c0a9f
jean <jeanpauldc90@gmail.com>
2026-07-20 14:59:34 -0500

    se realizo una modificación en la clase ScanDaoImpl para devolver el nombre del producto escaneado en getAdditivesReport

--- src/main/java/qalert/com/dao/ScanDaoImpl.java
+++ src/main/java/qalert/com/dao/ScanDaoImpl.java
@@ -100,6 +100,7 @@ public class ScanDaoImpl implements IScanDao {
 
                 header = new ScanHeaderResponse();
 
+                header.setProductName(DbUtil.getString(row, "product_name"));
                 header.setToxocityLevelId(DbUtil.getInteger(row, "toxicity_level_id"));
                 header.setToxocityLevel(DbUtil.getString(row, "toxicity_level"));
                 header.setTotal(DbUtil.getLong(row, "total").intValue());
